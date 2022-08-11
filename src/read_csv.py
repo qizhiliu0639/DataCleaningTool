6baf6130f502b94dc09270629bfd6a0affa3e566
liuzhiqi <j419230944@hotmail.com>
2022-08-11 17:56:22 +0800

    init

--- src/read_csv.py
+++ src/read_csv.py
@@ -6,12 +6,22 @@ def run():
     csvFile = open("../Resource/Result_8.csv", "r")
     reader = csv.reader(csvFile)
 
+    outputFile = open("../Output/output.csv","w")
+    writer = csv.writer(outputFile)
+
+    d = {
+        "信息工程": "Engineer",
+
+    }
     result = {}
     for item in reader:
         # 忽略第一行
-        if reader.line_num == 1:
-            continue
-        print(item)
+        # print(reader.line_num)
+        # if reader.line_num == 1:
+        #     continue
+        if item[6] == "信息工程":
+            item[6] = "Engineer"
+        writer.writerow(item)
 
     csvFile.close()
     print(result)
