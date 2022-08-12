0ef80d5f60a01bb8ea03ea813727a43d111d4641
liuzhiqi <j419230944@hotmail.com>
2022-08-12 17:22:56 +0800

    add mapping class.

--- src/read_csv.py
+++ src/read_csv.py
@@ -1,6 +1,21 @@
 import csv
 
 
+class mapping:
+    def __init__(self):
+        self.d = {}
+
+    def add(self, key ,value):
+        self.d[key] = value
+
+def create_mapping():
+    m = mapping()
+    d = {"信息工程": "EE",
+         "Software Engineer":"Engineer",
+         "信息系统与信息管理":"IS"}
+    for i in d:
+        m.add(i, d[i])
+    return m
 
 def run():
     csvFile = open("../Resource/Result_8.csv", "r")
@@ -9,22 +24,29 @@ def run():
     outputFile = open("../Output/output.csv","w")
     writer = csv.writer(outputFile)
 
-    d = {
-        "信息工程": "Engineer",
+    m = create_mapping()
 
-    }
-    result = {}
+    d_1 = set()
+    d_6 = set()
     for item in reader:
         # 忽略第一行
         # print(reader.line_num)
-        # if reader.line_num == 1:
-        #     continue
-        if item[6] == "信息工程":
-            item[6] = "Engineer"
+        if reader.line_num == 1:
+            writer.writerow(item)
+            continue
+        d_1.add(item[0])
+        d_6.add(item[6])
+        if item[6] in m.d:
+            item[6] = m.d[item[6]]
+        else:
+            item[6] = "other"
+
         writer.writerow(item)
 
+
     csvFile.close()
-    print(result)
+    print(d_6)
+    print(d_1)
 
 if __name__ == "__main__":
     run()
