2e5df2e48c0f0fc592cf26ad52c887d5573b1617
qizhiliu <j419230944@hotmail.com>
2022-08-17 00:25:30 +0800

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import tkinter as tk  # 使用Tkinter前需要先导入
+import tkinter
 import os
 from tkinter import filedialog, dialog
 window = tk.Tk()
@@ -11,34 +12,20 @@ window.geometry('600x400')
 text1 = tk.Text(window, width=50, height=10, bg='orange', font=('Arial', 12))
 text1.pack()
 
-def get_mapping(key, value):
-    key_2 = key.split(",")
-    value_2 = value.split(",")
-    d = {}
-    if len(key_2) != value_2:
-        return d
-
-    for i in range(len(key_2)):
-        d[key_2[i]] = value_2[i]
-
-    text1.insert(str(d))
-    print(d)
-    return d
-
-
-
-# def open_file():
-#     '''
-#         打开文件
-#         :return:
-#         '''
-#     global file_path
-#     global file_text
-#     file_path = filedialog.askopenfilename(title=u'选择文件', initialdir=(os.path.expanduser('H:/')))
-#     print('打开文件：', file_path)
-#     if file_path is not None:
-#         with open(file=file_path, mode='r+', encoding='utf-8') as file:
-#             file_text = file.read()
+d = {}
+def open_file():
+    '''
+        打开文件
+        :return:
+        '''
+    global file_path
+    global file_text
+    file_path = filedialog.askopenfilename(title=u'选择文件', initialdir=(os.path.expanduser('H:/')))
+    file_path = filedialog.asksaveasfilename(title=u'保存文件')
+    print('打开文件：', file_path)
+    if file_path is not None:
+        with open(file=file_path, mode='r+', encoding='utf-8') as file:
+            file_text = file.read()
 
 def run():
 
@@ -55,14 +42,29 @@ def run():
     # e5.pack()
     # e6.pack()
 
+    def get_mapping():
+        global ind
+        key = e2.get()
+        value = e4.get()
+        key = key.split(",")
+        value = value.split(",")
 
-    # bt1 = tk.Button(window, text='打开文件', width=15, height=2, command=open_file)
-    # bt1.pack()
-    bt2 = tk.Button(window, text='test', width=15, height=2, command=get_mapping(e2.get(), e4.get()))
+        if len(key) != len(value):
+            tkinter.messagebox.showerror(title='Error', message='key 和 value长度不一致')
+
+        for i in range(len(key)):
+            d[key[i]] = value[i]
+        text1.insert(tk.INSERT, d)
+        ind += 1
+
+    bt1 = tk.Button(window, text='打开文件', width=15, height=2, command=open_file)
+    bt1.pack()
+    bt2 = tk.Button(window, text='test', width=15, height=2, command=get_mapping)
     bt2.pack()
 
     window.mainloop()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    ind = 0
     run()
