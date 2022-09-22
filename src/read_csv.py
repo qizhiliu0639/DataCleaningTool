c664405e862ea2f1b212d4b363b9f11120c6526e
qizhiliu <j419230944@hotmail.com>
2022-09-22 22:51:26 +0800

    update

--- src/read_csv.py
+++ src/read_csv.py
@@ -17,11 +17,19 @@ def create_mapping():
         m.add(i, d[i])
     return m
 
+def extract_offer_information(item):
+    if item[0].lower() == 'o' or item[0].lower() == 'o':
+        item[0] = 1
+    else:
+        item[0] = 0
+    return item
+
+
 def run():
-    csvFile = open("../Resource/Result_8.csv", "r")
+    csvFile = open("../Resource/Result_8.csv", "r", encoding='utf-8')
     reader = csv.reader(csvFile)
 
-    outputFile = open("../Output/output.csv","w")
+    outputFile = open("../Output/NTU_CCA.csv","w", encoding='utf-8')
     writer = csv.writer(outputFile)
 
     m = create_mapping()
@@ -29,19 +37,17 @@ def run():
     d_1 = set()
     d_6 = set()
     for item in reader:
+        # print(item)
         # 忽略第一行
         # print(reader.line_num)
         if reader.line_num == 1:
             writer.writerow(item)
             continue
-        d_1.add(item[0])
-        d_6.add(item[6])
-        if item[6] in m.d:
-            item[6] = m.d[item[6]]
-        else:
-            item[6] = "other"
-
-        writer.writerow(item)
+        d_1.add(item[4])
+        if item[2][:7] == 'Nanyang' and item[3][0].lower() == 'c':
+            print(item)
+            item = extract_offer_information(item)
+            writer.writerow(item)
 
 
     csvFile.close()
